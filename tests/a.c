68165652bdfbc1462f672336fb3557c6557e8164
MaximLeontev <maximricardo8@gmail.com>
2025-06-18 00:07:25 +0200

    changed test program to use 32 bit ints

--- tests/a.c
+++ tests/a.c
@@ -1,15 +1,15 @@
-typedef char i8;
+typedef int i32;
 
 int strtol(char *str, char **end, int base);
 /* a hacky way to get variadic functions working for now */
 int printf();
 
-i8 fibonacci(i8 n);
+i32 fibonacci(i32 n);
 
 int main(int argc, char **argv) {
 
     char *end_ptr;
-    i8 n;
+    i32 n;
 
     if (argc < 2) {
         printf("give an argument containing which fibonacci number to get.\n");
@@ -21,13 +21,13 @@ int main(int argc, char **argv) {
 
 }
 
-i8 fibonacci(i8 n) {
+i32 fibonacci(i32 n) {
 
-    i8 a = 0;
-    i8 b = 1;
-    i8 c;
+    i32 a = 0;
+    i32 b = 1;
+    i32 c;
 
-    i8 i = 0;
+    i32 i = 0;
 
     if (n < 2)
         return n;
