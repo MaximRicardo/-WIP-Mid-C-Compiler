428974593b7bec8345d4fe63f3023395471835e5
MaximLeontev <maximricardo8@gmail.com>
2025-06-24 14:39:31 +0200

    finally got around to refactoring the compile func in main.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "ast.h"
 #include "code_gen.h"
 #include "comp_args.h"
 #include "file_io.h"
@@ -14,7 +13,7 @@
 #include "bin_to_unary.h"
 #include "merge_strings.h"
 #include "pre_proc.h"
-#include "structs.h"
+#include "transl_unit.h"
 
 #define m_build_bug_on(condition) \
     ((void)sizeof(char[1 - 2*!!(condition)]))
@@ -34,71 +33,64 @@ static char *read_file(const char *file_path) {
 
 }
 
+static void free_macros(struct PreProcMacroList *macros,
+        struct MacroInstList *macro_insts) {
+
+    while (macro_insts->size > 0)
+        MacroInstList_pop_back(macro_insts, MacroInstance_free);
+    MacroInstList_free(macro_insts);
+
+    while (macros->size > 0)
+        PreProcMacroList_pop_back(macros, PreProcMacro_free);
+    PreProcMacroList_free(macros);
+
+}
+
+/* preprocessing */
 static void compile(char *src, FILE *output, bool *error_occurred) {
 
     struct PreProcMacroList macros;
     struct MacroInstList macro_insts;
-    PreProc_process(src, &macros, &macro_insts, CompArgs_args.src_path);
+
+    struct Lexer lexer;
+    struct TranslUnit tu;
+
     *error_occurred = false;
 
-    if (!PreProc_error_occurred) {
-        struct Lexer lexer = Lexer_lex(src, CompArgs_args.src_path, &macro_insts);
-
-        if (!Lexer_error_occurred) {
-            struct TranslUnit tu;
-
-            MergeStrings_merge(&lexer.token_tbl);
-            BinToUnary_convert(&lexer.token_tbl);
-            PreToPostFix_convert(&lexer.token_tbl);
-
-            tu = Parser_parse(&lexer);
-
-            if (!Parser_error_occurred && output) {
-                if (CompArgs_args.optimize) {
-                    /* no optional optimizations yet */
-                }
-                CodeGen_generate(output, tu.ast, tu.structs);
-            }
-            else
-                *error_occurred = true;
-
-            while (tu.structs->size > 0) {
-
-#ifdef m_print_structs
-                struct Struct back = StructList_back(tu.structs);
-                u32 i;
-
-                printf("struct %s {\n", back.name);
-                for (i = 0; i < back.members.size; i++) {
-                    printf("\t(%d) %s; //lvls of indir %u, offset %u\n",
-                            back.members.elems[i].type,
-                            back.members.elems[i].name,
-                            back.members.elems[i].lvls_of_indir,
-                            back.members.elems[i].offset);
-                }
-                printf("};\n");
-#endif
-
-                StructList_pop_back(tu.structs, Struct_free);
-            }
-            StructList_free(tu.structs);
-            BlockNode_free_w_self(tu.ast);
-        }
-        else
-            *error_occurred = true;
-
-        Lexer_free(&lexer);
+    PreProc_process(src, &macros, &macro_insts, CompArgs_args.src_path);
+    if (PreProc_error_occurred) {
+        free_macros(&macros, &macro_insts);
+        return;
     }
-    else
-        *error_occurred = true;
 
-    while (macro_insts.size > 0)
-        MacroInstList_pop_back(&macro_insts, MacroInstance_free);
-    MacroInstList_free(&macro_insts);
+    lexer = Lexer_lex(src, CompArgs_args.src_path, &macro_insts);
+    if (Lexer_error_occurred) {
+        Lexer_free(&lexer);
+        free_macros(&macros, &macro_insts);
+        return;
+    }
 
-    while (macros.size > 0)
-        PreProcMacroList_pop_back(&macros, PreProcMacro_free);
-    PreProcMacroList_free(&macros);
+    MergeStrings_merge(&lexer.token_tbl);
+    BinToUnary_convert(&lexer.token_tbl);
+    PreToPostFix_convert(&lexer.token_tbl);
+
+    tu = Parser_parse(&lexer);
+    if (Parser_error_occurred || !output) {
+        TranslUnit_free(tu);
+        Lexer_free(&lexer);
+        free_macros(&macros, &macro_insts);
+        return;
+    }
+
+    if (CompArgs_args.optimize) {
+        /* no optional optimizations yet */
+    }
+
+    CodeGen_generate(output, tu.ast, tu.structs);
+
+    TranslUnit_free(tu);
+    Lexer_free(&lexer);
+    free_macros(&macros, &macro_insts);
 
 }
 
