68876592ddeb3884ee6ea658734512ae14336513
MaximLeontev <maximricardo8@gmail.com>
2025-07-01 21:25:17 +0200

    added another clause to the m_warning macro in case neither GCC, CLang, nor MSVC is used.

--- src/utils/macros.h
+++ src/utils/macros.h
@@ -11,10 +11,13 @@
 #define m_emit_compiler_warning_stringify1(x) m_emit_compiler_warning_stringify0(x)
 #ifdef __GNUC__
     #define m_emit_compiler_warning_compose(x) GCC warning x
-#else
+#elif defined (_MSC_VER)
     #define m_emit_compiler_message_preface(type) \
     __FILE__ "(" m_emit_compiler_warning_stringify1(__LINE__) "): " type ": "
     #define m_emit_compiler_warning_compose(x) message(m_emit_compiler_message_preface("warning C0000") x)
+#else
+/* just give up if the compiler being used supports neither GCC or MSVC shit */
+    #define m_emit_compiler_warning_compose(x)
 #endif
 #define m_warning(x) _Pragma(m_emit_compiler_warning_stringify1(m_emit_compiler_warning_compose(x)))
 
